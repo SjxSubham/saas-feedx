b03f17e72c0deac465ce4918addb3850255b3d8c
SjxSubham <sjxsubham@gmail.com>
2024-11-07 01:35:46 +0530

    changed

---
+++ components/new.proj.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+const NewProjBtn = () => {
+    return (
+        <div className="flex justify-center">
+        <Button className="bg-black rounded-md">New Project</Button>
+        </div>
+    );
+};
