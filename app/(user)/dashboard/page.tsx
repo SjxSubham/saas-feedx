950b8546ece6174fa27feb9ce455d7e33c8aa5b2
SjxSubham <sjxsubham@gmail.com>
2024-11-26 23:51:02 +0530

    page.tsx

--- app/(user)/dashboard/page.tsx
+++ app/(user)/dashboard/page.tsx
@@ -1,5 +1,20 @@
-export default function Page(){
+import NewProjBtn from "@/components/new.proj";
+import { Button } from "@/components/ui/button";
+import {db} from "@/db";
+import { projects} from "@/db/schema";
+import {auth, currentUser} from "@clerk/nextjs/server";
+
+import { log } from "console";
+export default async function Page(){
+    const { userId } = auth();
+    
+
+    const allProjects = await db.select().from(projects);
+   
+    
+    
     return (
-        <div>dashboard page</div>
+       <div><NewProjBtn/></div>
+
     )
 }
