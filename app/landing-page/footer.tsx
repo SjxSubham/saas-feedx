5b4ae6baa995bc00b940eceb876a7b49fd3b91ce
SjxSubham <sjxsubham@gmail.com>
2024-12-22 10:08:36 +0530

    footer fixed

--- app/landing-page/footer.tsx
+++ app/landing-page/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="rounded-lg shadow m-4 mt-28 border-t p-2 flex flex-wrap w-full mx-auto max-w-screen-xl bg-gray-100 ">
+    <footer className=" rounded-lg shadow -m-20 mt-28 border-t p-2 flex flex-wrap w-full mx-auto max-w-screen-xl bg-gray-100 ">
       <div className="container mx-auto text-center">
         <h2 className="text-lg font-semibold text-gray-800">FeedX</h2>
         <p className="text-sm text-gray-500 mt-2">
