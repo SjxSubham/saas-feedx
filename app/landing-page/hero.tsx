9d4fdf6a0969a65190fa32f196ccd911242b140f
SjxSubham <sjxsubham@gmail.com>
2025-03-14 21:27:31 +0530

    shadow

--- app/landing-page/hero.tsx
+++ app/landing-page/hero.tsx
@@ -39,7 +39,7 @@ const Hero = () => {
             </SignedIn>
           </div>
         </div>
-        <div className="flex-1 max-w-md shadow-sm hover:shadow-cyan-300">
+        <div className="flex-1 max-w-md shadow-md">
           <Image src={'/feedx.gif'} alt="demo" layout={'responsive'} width={155} height={155} unoptimized={true} />
         </div>
       </div>
