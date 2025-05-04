ac2d35b1b539b62aa45060cd4d2c04871fa6030b
SjxSubham <sjxsubham@gmail.com>
2025-05-04 20:28:26 +0530

    little bit colour change

--- app/landing-page/pricing-section.tsx
+++ app/landing-page/pricing-section.tsx
@@ -55,8 +55,8 @@ export const pricingPlans: PricingPlan[] = [
 const PricingSection = () => {
   return (
     <div className="text-center">
-      <h1 className="capitalize text-3xl">Pricing</h1>
-      <h2 className="font-extrabold text-3xl mb-8 pt-3">
+      <h1 className="capitalize text-white font-serif text-3xl">Pricing</h1>
+      <h2 className="font-extrabold text-white text-3xl mb-8 pt-3">
         Flexible Pricing to Fit Your Needs
       </h2>
       <div className="mt-10 grid items-center grid-cols-1 gap-3 md:grid-cols-3 max-w-screen-xl">
